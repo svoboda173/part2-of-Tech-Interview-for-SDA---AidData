3624e9f12093921ce8a4341465027e24376708ff
Eli Svoboda <essvoboda@email.wm.edu>
2023-03-24 17:12:11 +0100

    Working!

--- src/App.js
+++ src/App.js
@@ -1,56 +1,119 @@
-import logo from './aiddata.png';
+import logo from "./aiddata.png";
+import { useState } from "react";
+import "./App.css";
 
-import './App.css';
-
-//TODO: add aiddata logo?
-
-function App() {
+function UserInfo({ userName, userAge, userFavoriteSeason, userDrives }) {
   return (
-    <div className = "App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-
-        <h4>  Please enter the following information about yourself:</h4>
-
-        <label>
-          Name:
-          <input type="text" required maxLength={150}/>
-        </label>
-        
-        <label>
-          Age:
-          <input type="number" required max={125}/>
-        </label>
-
-        <label>
-          Favorite season:
-          <select required>
-            <option>Spring </option>
-            <option>Summer </option>
-            <option>Fall </option>
-            <option>Winter </option>
-          </select>
-        </label>
-
-        <fieldset>
-          <legend>Can you drive a car?</legend>
-          <label>
-            Yes
-            <input required type="radio" name = "drives?" value = "yes"/>
-          </label>
-
-          <label className="noSelect">
-            No
-            <input required type="radio" name = "drives?" value = "no"/>
-          </label>
-        </fieldset>
-        <br/>
-
-        <input type = "submit" value = "Submit"/>
-
-      </header>
+    <div>
+      <br />
+      The User Entered:
+      <p>
+        Name: {userName} <br />
+        Age: {userAge} <br />
+        Favorite Season: {userFavoriteSeason} <br />
+        {userDrives ? "The user drives" : "The user does not drive"} <br />
+      </p>
     </div>
   );
 }
 
-export default App;
+export default function App() {
+  // create state variables for the info we need to save to display eventually
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [favSeason, setFavSeason] = useState("Spring");
+  const [drives, setDrives] = useState(false);
+  const [hasSubmitted, setHasSubmitted] = useState(false);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setHasSubmitted(true);
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <form>
+          <img src={logo} className="App-logo" alt="logo" />
+
+          <h4> Please enter the following information about yourself:</h4>
+
+          <label>
+            Name:
+            <input
+              type="text"
+              id="name"
+              value={name}
+              required
+              maxLength={150}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </label>
+          <br />
+
+          <label>
+            Age:
+            <input
+              type="number"
+              id="age"
+              value={age}
+              required
+              max={125}
+              onChange={(e) => setAge(e.target.value)}
+            />
+          </label>
+          <br />
+
+          <label>
+            Favorite season:
+            <select
+              required
+              value={favSeason}
+              onChange={(e) => setFavSeason(e.target.value)}
+            >
+              <option>Spring </option>
+              <option>Summer </option>
+              <option>Fall </option>
+              <option>Winter </option>
+            </select>
+          </label>
+          <br />
+
+          <fieldset>
+            <legend>Can you drive a car?</legend>
+            <label>
+              Yes
+              <input
+                required
+                type="radio"
+                name="drives?"
+                value="yes"
+                checked={drives}
+                onChange={(e) => setDrives(!drives)}
+              />
+            </label>
+
+            <label className="noSelect">
+              No
+              <input
+                required
+                type="radio"
+                name="drives?"
+                value="no"
+                checked={!drives}
+                onChange={(e) => setDrives(!drives)}
+              />
+            </label>
+          </fieldset>
+          <br />
+
+          <input type="submit" value="Submit" onClick={handleSubmit} />
+
+          <div>
+            {hasSubmitted && <UserInfo userName={name} userAge={age} userFavoriteSeason={favSeason} userDrives={drives}/>}
+          </div>
+        </form>
+      </header>
+    </div>
+  );
+}
